f3c8065d0b34130c33308721507a9b8fabebfc44
kkabdol <kkabdol@gmail.com>
2013-09-11 22:20:41 +0900

    Initial Commit

---
+++ XorDouble/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  XorDouble
+//
+//  Created by Shaun Choe on 9/11/13.
+//  Copyright (c) 2013 Shaun Choe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
